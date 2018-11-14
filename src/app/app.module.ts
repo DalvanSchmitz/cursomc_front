518c756e98edd16d504a25b13228e49b283e6eef
Dalvan Schmitz <dalvan.schmitz@gmail.com>
2018-11-14 08:38:43 -0200

    requisição para categoria da api Spring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MyApp } from './app.component';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { categoriaService } from '../services/domain/categoria.service';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    categoriaService
   ]
 })
 export class AppModule {}
---
+++ src/services/domain/categoria.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs/RX";
+import { CategoriaDTO } from "../../models/categoria.DTO";
+import { API_CONFIG } from "../../config/api.config";
+
+Injectable
+export class categoriaService{
+
+    constructor(public http: HttpClient){
+    }
+
+    public findAll(): Observable<CategoriaDTO[]>{
+        return this.http.get<CategoriaDTO[]>(`$(API_CONFIG.baseUrl)/categorias`);
+    }
+}
